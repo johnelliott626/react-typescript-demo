e3cb3ee85bfa879cb6d790f1633f38948499c125
John Elliott <jelliott@redhat.com>
2026-07-14 12:31:24 -0400

    task 2 implementation snazyyyyy

--- src/components/ColoredTd.tsx
+++ src/components/ColoredTd.tsx
@@ -2,7 +2,7 @@ import { Td, TdProps } from '@patternfly/react-table';
 import { createUseStyles } from 'react-jss';
 import { Color } from 'src/api/CustomerApi';
 
-const useStyles = (color: any) => // TODO: Use the new Color type to fix this.
+const useStyles = (color: Color) =>
   createUseStyles({
     withColor: {
       color,
--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -20,6 +20,7 @@ import { useQuery } from 'react-query';
 import { choosableColors, Color, Customer, getCustomers } from 'src/api/CustomerApi';
 import { ColoredTd } from 'src/components/ColoredTd';
 import Loader from 'src/components/Loader';
+import { SnazzyButton } from 'src/components/SnazzyButton';
 import { useAppContext } from 'src/middleware';
 
 const useStyles = createUseStyles({
@@ -117,7 +118,7 @@ export default () => {
             onChange={(value) => setNewUser({ ...newUser, isCool: value })}
             isChecked={newUser.isCool}
           />
-          <Button type='submit'>Submit</Button>
+          <SnazzyButton type='submit' isSnazzy={true}>Submit</SnazzyButton>
         </Form>
       </Modal>
       <Grid>
